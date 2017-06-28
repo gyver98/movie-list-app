282e1b4a5ef7954d7214bdbcd7faa8773757d845
Matthew Choi <matthew.choi@accenture.com>
2017-06-28 14:31:04 +1000

    style of movie component

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Home from './components/Home';
+import Movie from './components/Movie';
+
+import Router from 'react-router/BrowserRouter';
+import Match from 'react-router/Match';
+import Link from 'react-router/Link';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <div className="App-header">
+            <h2>Flix</h2>
+            <Link to="/">Home</Link>
+          </div>
+          <div className="container">
+            <Match exactly pattern="/" component={Home} />
+            <Match pattern="/movies/:movieId" component={Movie} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
